dd78a1cf8be008315965354ee4918e2916eca213
Ben Pearey <bpearey@amplience.com>
2019-05-21 11:24:57 +0100

    build(app setup): initial nuxtjs implementation

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['js', 'ts', 'vue', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/components/**/*.vue', '<rootDir>/pages/**/*.vue']
+};
