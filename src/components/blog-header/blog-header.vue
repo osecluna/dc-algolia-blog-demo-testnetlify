7b158d5b7b5626e63c65f0172a655ca7717e69a2
Ben Pearey <bpearey@amplience.com>
2019-07-25 12:51:10 +0100

    refactor(header): moving header out into seperate component

---
+++ src/components/blog-header/blog-header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Dynamic Content Algolia Blog</h1>
+    <h5>Demonstraing how to integrate Amplience Dynamic Content with Algolia to create a simple blog</h5>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+class BlogHeader extends Vue {}
+
+export default BlogHeader;
+</script>
