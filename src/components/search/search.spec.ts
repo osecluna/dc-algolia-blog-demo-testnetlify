37556c2dc534dd43a9dde8680f2ae8ac74590774
klankester <klankester@amplience.com>
2019-07-29 15:49:28 +0100

    feat(search.spec): trying to get pagination test working

--- src/components/search/search.spec.ts
+++ src/components/search/search.spec.ts
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import Search from './search.vue';
 import searchResponse from './fixtures/search-response.json';
+import searchResponseMutli from './fixtures/search-response-multiple-pages.json';
 import flushPromises from 'flush-promises';
 import { MultiResponse, Response } from '~/node_modules/@types/algoliasearch';
 
@@ -25,19 +26,34 @@ describe('Search', () => {
     }
   );
 
-  test('is a search result with pagination', async () => {
+  test('is a search result', async () => {
     mockSearchFn.mockImplementationOnce(
       async (): Promise<MultiResponse<Response>> => {
         return { results: [searchResponse] };
       }
     );
-
     const wrapper = mount(Search);
     await flushPromises();
-
     expect(mockSearchFn).toBeCalled();
-
     expect(wrapper.isVueInstance()).toBeTruthy();
     expect(wrapper.element).toMatchSnapshot();
+    wrapper.destroy();
   });
+
+  xtest('is a search result with pagination', async () => {
+    mockSearchFn.mockImplementationOnce(
+      async (): Promise<MultiResponse<Response>> => {
+        return { results: [searchResponseMutli] };
+      }
+    );
+    const wrapper = mount(Search);
+    await flushPromises();
+    const secondPage = wrapper.findAll('.ais-Pagination-link').at(1);
+    secondPage.trigger('click');
+    expect(mockSearchFn).toBeCalled();
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.element).toMatchSnapshot();
+    wrapper.destroy();
+  });
+
 });
