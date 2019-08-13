22a76290d241a11a727bbd680f6e2639ce86aa90
Sean Ives <sives@amplience.com>
2019-08-13 17:13:55 +0100

    feat(algolia blog post): fix styling of card

--- src/components/card/card.vue
+++ src/components/card/card.vue
@@ -5,7 +5,7 @@
         <img class="image" :src="image" :alt="title || 'No image.'" />
         <div class="content">
           <h2>{{ title || 'No title.' }}</h2>
-          <h2>{{ description || 'No description.' }}</h2>
+          <div class="description">{{ description || 'No description.' }}</div>
         </div>
       </nuxt-link>
     </div>
@@ -46,7 +46,7 @@ export default Card;
 </script>
 
 <style lang="scss">
-$titleLineHeight: 22px;
+$titleLineHeight: 12px;
 $titleLinesToDisplay: 3;
 
 h2 {
@@ -67,7 +67,7 @@ h2 {
   overflow: hidden;
   width: 100%;
 }
-.date-time {
+.description {
   font-size: 13px;
   color: #999;
 }
